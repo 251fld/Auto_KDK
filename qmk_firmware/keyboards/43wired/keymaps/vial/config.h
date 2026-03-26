5d3bed6aedef5ff7acd0ebef7c1432ab07c173d2
251fld <251fld@4square.ponytail.jp>
2026-03-26 16:31:32 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/43wired/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xc0, 0x05, 0xa9, 0xcc, 0x15, 0xef, 0x32, 0x62}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
